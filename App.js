8859ff0a2bba86504d0f27c58685441b875ac728
damon.chen <chj_0507_dz@sina.com>
2023-09-19 10:57:41 +0800

    feat: 项目中使用expo-image

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Image} from 'expo-image';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/icon.png')} style={{width: 300, height: 300}} />
     </View>
   );
 }
